04bf5a04f997c23600abd7b483200ac2b4d65bc6
Bhuvana57 <130349892+Bhuvana57@users.noreply.github.com>
2025-12-27 11:27:41 +0530

    Implement ProfilerImpl with dynamic proxy and file writing

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -1,11 +1,17 @@
 package com.udacity.webcrawler.profiler;
 
 import javax.inject.Inject;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
@@ -29,17 +35,32 @@ final class ProfilerImpl implements Profiler {
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
 
-    // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
-    //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
-    //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
+    // Check if the class has any @Profiled methods
+    boolean hasProfiledMethod = Arrays.stream(klass.getDeclaredMethods())
+        .anyMatch(method -> method.isAnnotationPresent(Profiled.class));
 
-    return delegate;
+    if (!hasProfiledMethod) {
+      throw new IllegalArgumentException("Class " + klass.getName() + " has no @Profiled methods");
+    }
+
+    // Create and return a dynamic proxy
+    @SuppressWarnings("unchecked")
+    T proxy = (T) Proxy.newProxyInstance(
+        klass.getClassLoader(),
+        new Class<?>[]{klass},
+        new ProfilingMethodInterceptor(clock, delegate, state)
+    );
+
+    return proxy;
   }
 
   @Override
   public void writeData(Path path) {
-    // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
-    //       path, the new data should be appended to the existing file.
+    try (BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+      writeData(writer);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to write profiling data to file: " + path, e);
+    }
   }
 
   @Override
