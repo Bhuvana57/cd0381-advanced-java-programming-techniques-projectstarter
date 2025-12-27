deee9ead99d0b35502af5e7b3566eaa2d82ae898
Bhuvana57 <130349892+Bhuvana57@users.noreply.github.com>
2025-12-27 11:32:06 +0530

    Update WebCrawlerMain.java

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -36,8 +36,27 @@ public final class WebCrawlerMain {
 
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+
+        // Write the crawl results to a JSON file (or System.out if the file name is empty)
+        if (!config.getResultPath().isEmpty()) {
+                Path resultPath = Path.of(config.getResultPath());
+                resultWriter.write(resultPath);
+              } else {
+                Writer writer = new BufferedWriter(new OutputStreamWriter(System.out));
+                resultWriter.write(writer);
+                writer.flush();
+              }
+
+        // Write the profile data to a text file (or System.out if the file name is empty)
+        if (!config.getProfileOutputPath().isEmpty()) {
+                Path profilePath = Path.of(config.getProfileOutputPath());
+                profiler.writeData(profilePath);
+              } else {
+                Writer writer = new BufferedWriter(new OutputStreamWriter(System.out));
+                profiler.writeData(writer);
+                writer.flush();
+              }
+
   }
 
   public static void main(String[] args) throws Exception {
